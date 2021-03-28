02c6659949b217ae4aadffc99c5c737d4bc82e1a
MadalinaPlugariu <61787426+MadalinaPlugariu@users.noreply.github.com>
2021-03-28 21:57:28 +0300

    course5_homework1

---
+++ Course5/Homework1/Homework1/DummyDatabase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+    class DummyDatabase
+    {
+        private bool IsConnected = false;
+        public List<string> WordsSaved = new List<string>();
+        public class DummyDatabaseConnectionException : Exception
+        {
+            public DummyDatabaseConnectionException(string message) : base(message)
+            {
+
+            }
+        }
+        public void Connect()
+        {
+            Random r = new Random();
+            int rand = r.Next(1, 6);
+            try
+            {
+                if (rand == 1)
+                {
+
+                    IsConnected = false;
+                    throw new DummyDatabaseConnectionException("DummyDatabaseConnectionException. " + " Failed trying to connect. Try again");
+                }
+                IsConnected = true;
+                Console.WriteLine("Connection established");
+
+            }
+            catch (DummyDatabaseConnectionException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finish trying to connect");
+            }
+
+
+        }
+
+        public void Disconnect()
+        {
+            IsConnected = false;
+            Console.WriteLine("You have been disconnected.");
+        }
+        public void Save(string wordToSave)
+        {
+            if (IsConnected == true)
+            {
+                if (WordsSaved.Count < 5)
+                    WordsSaved.Add(wordToSave);
+                else
+                {
+                    Console.WriteLine("The DummyDatabase is full.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("It's not connected");
+            }
+        }
+    }
+}
---
+++ Course5/Homework1/Homework1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DummyDatabase d = new DummyDatabase();
+            d.Connect();
+            d.Save("Buna");
+            d.Disconnect();
+
+            d.Connect();
+            d.Save("Hei");
+            d.Disconnect();
+
+            d.Connect();
+            d.Save("Salut");
+            d.Disconnect();
+
+            d.Connect();
+            d.Save("Hello");
+            d.Disconnect();
+
+            d.Connect();
+            d.Save("Good bye!");
+            d.Disconnect();
+
+            d.Connect();
+            d.Save("Adios!");
+            d.Disconnect();
+
+            foreach (var l in d.WordsSaved)
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
