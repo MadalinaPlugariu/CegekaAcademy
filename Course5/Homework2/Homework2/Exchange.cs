9172e801ae9402072eea27f199d5974420fb5ec2
MadalinaPlugariu <61787426+MadalinaPlugariu@users.noreply.github.com>
2021-03-28 21:58:25 +0300

    course5_homework2

---
+++ Course5/Homework2/Homework2/Exchange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework2
+{
+  public class Exchange<T>
+    {
+
+        public T[] Swap(T[] element, int firstIndex, int lastIndex)
+        {
+
+            var temp = element[firstIndex];
+            element[firstIndex] = element[lastIndex];
+            element[lastIndex] = temp;
+
+            return element;
+        }
+
+          public void PrintArray(T[] element)
+          {
+            for (int i = 0; i < element.Length; i++)
+                Console.WriteLine(element[i]);
+          }
+    }
+}
---
+++ Course5/Homework2/Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exchange<int> array = new Exchange<int>();
+
+            int[] arr = { 1, 5, 3, 7, 4, 6 };
+
+            array.Swap(arr,  1, 3);
+            array.PrintArray(arr);
+
+        }
+    }
+}
