0f2bbd662a287c89ff2e080f284e6614b6cd5fb5
MadalinaPlugariu <61787426+MadalinaPlugariu@users.noreply.github.com>
2021-03-28 21:58:51 +0300

    course5_homework3

---
+++ Course5/Homework3/Homework3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolarSystem s = new SolarSystem();
+            s.AddNewPlanetAtTheEnd("Earth");
+            s.AddNewPlanetAtTheEnd("Mars");
+            s.AddNewPlanetAtTheEnd("Uranus");
+            s.AddNewPlanetAtTheEnd("Venus");
+            s.AddNewPlanetAtTheEnd("Jupiter");
+
+            s.InsertNewPlanet("Impostor", 1);
+
+            s.ReplacePlanet("Moon", 3);
+
+            s.RemovePlanet(2);
+
+            s.SwapPlanets(0, 2);
+
+            s.ReverseOrder();
+            
+            s.Print();
+
+            //s.ReadOnly();
+        }
+    }
+}
---
+++ Course5/Homework3/Homework3/SolarSystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Homework3
+{
+    class SolarSystem
+    {
+        private List<string> universe = new List<string>();
+        public ReadOnlyCollection<string> UniverseRead
+        {
+            get
+            {
+                return universe.AsReadOnly();
+            }
+        }
+        public List<string> Universe { get; private set; }
+       
+
+        public void AddNewPlanetAtTheEnd(string planet)
+        {
+            universe.Add(planet);
+        }
+        public void InsertNewPlanet(string planet, int index)
+        {
+            universe.Insert(index, planet);
+        }
+        public void RemovePlanet(int index)
+        {
+            universe.RemoveAt(index);
+        }
+        public void ReplacePlanet(string planet, int index)
+        {
+            universe[index] = planet;
+
+        }
+        public void SwapPlanets(int firstIndex, int secondIndex)
+        {
+            var aux = universe[firstIndex];
+            universe[firstIndex] = universe[secondIndex];
+            universe[secondIndex] = aux;
+        }
+        public void ReverseOrder()
+        {
+            universe.Reverse();
+        }
+        public void ReadOnly()
+        {
+           /* IList<string> universeRead = universe.AsReadOnly();*/
+            foreach(var i in UniverseRead)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void Print()
+        {
+            foreach (var i in universe)
+                Console.WriteLine(i);
+        }
+
+    }
+}
